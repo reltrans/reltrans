efa47617a30a913adc9b3048be1640d515a0d307
Matteo Lucchini <matteol@mit.edu>
2022-03-01 10:13:01 -0500

    Added double LP simulation model

--- srevtrans.f90
+++ srevtrans.f90
@@ -410,6 +410,179 @@ end subroutine tdrtdistX
 !-----------------------------------------------------------------------
 
 !-----------------------------------------------------------------------
+subroutine simrtdbl(ear, ne, param, ifl, photar)
+  use telematrix
+  implicit none
+  integer :: ne, ifl, Cp, dset, nlp, i
+  real    :: ear(0:ne), param(26), photar(ne), par(28)
+  real    :: gammac2, Texp, E, dE, getcountrate
+  real    :: rephotar(ne), imphotar(ne)
+  real, parameter :: Emin = 1e-1, Emax = 300.0
+  integer, parameter :: nex=2**12
+  real :: earx(0:nex),photarx(nex),pow
+  real :: Pr,rephotarx(nex),imphotarx(nex),mur,mus
+  real :: dlag(ne),G2,ReG,ImG,Psnoise,Prnoise,br,bs(ne)
+  real :: flo,fhi,fc,lag(ne),gasdev,lagsim(ne)
+  real, parameter :: pi = acos(-1.0)
+  integer idum, unit,xunit,status,j
+  real E1,E2,frac
+  data idum/-2851043/
+  save idum
+  character (len=200) command,flxlagfile,phalagfile,rsplagfile,lagfile,root
+! Settings
+  Cp   = 2   !|Cp|=2 means nthcomp, Cp>1 means there is a density parameter     
+  dset = 0   !dset=1 means distance is set, logxi is calculated internally
+  nlp = 2    !use a single lamp post
+! Transfer to general parameter array
+  par(1)  = param(1)         !h1
+  par(2)  = param(2)         !h2
+  par(3)  = param(3)         !a
+  par(4)  = param(4)         !inc
+  par(5)  = param(5)         !rin
+  par(6)  = param(6)         !rout
+  par(7)  = param(7)         !zcos
+  par(8)  = param(8)         !Gamma
+  par(9)  = param(9)         !logxi
+  par(10) = param(10)        !Afe
+  par(11) = param(11)        !lognep
+  par(12) = param(12)        !kTe
+  par(13) = param(13)        !lumratio
+  par(14) = param(14)        !Nh
+  par(15) = param(15)        !boost
+  par(16) = 1.0              !qboost
+  par(17) = param(16)        !Mass
+  par(18) = 0.0              !honr
+  par(19) = 0.0              !b1
+  par(20) = 0.0              !b2
+  par(21) = param(17)        !floHz
+  par(22) = param(18)        !fhiHz
+  gammac2 = param(19)        !squared coherence
+  par(24) = param(20)        !DelA
+  par(25) = param(21)        !DelAB
+  par(26) = param(22)        !g
+  par(27) = 1.0              !Anorm
+  Texp    = param(23)        !Texp (s)
+  pow     = param(24)        !power in [rms/mean]^2/Hz units (alpha(nu))
+  par(28) = param(25)        !telescope response
+  
+  flo = par(21)
+  fhi = par(22)
+  fc  = 0.5 * ( fhi + flo )
+  
+! Get `folded' lags
+  par(23) = 6.0   !ReIm
+  call genreltrans(Cp, dset, nlp, ear, ne, par, ifl, photar)  
+  do i = 1,ne
+     lag(i) = photar(i) / ( ear(i) - ear(i-1) )
+  end do
+  
+! Set internal energy grid
+  do i = 0, nex
+     earx(i) = Emin * (Emax/Emin)**(float(i)/float(nex))
+  end do
+  
+! Calculate real and imaginary parts on the fine energy grid
+  par(23) = 1.0   !ReIm
+  call genreltrans(Cp, dset, nlp, earx, nex, par, ifl, rephotarx)  
+  par(23) = 2.0   !ReIm
+  call genreltrans(Cp, dset, nlp, earx, nex, par, ifl, imphotarx)  
+! Get DC component
+  par(21) = 0.0   !floHz
+  par(22) = 0.0   !fhiHz
+  par(23) = 1.0   !ReIm
+  call genreltrans(Cp, dset, nlp, earx, nex, par, ifl, photarx)  
+
+! Read in background array
+  if( needbkg )then
+     allocate(bkgcounts(1:numchn))
+     allocate(bkgrate(1:numchn))
+     call readinbkg
+     needbkg = .false.
+  end if
+
+! Calculate background in reference band
+  br = 0.0
+  do i = ilo,ihi
+     br = br + bkgrate(i)
+  end do
+
+! Calculate background in subject
+  do j = 1,ne
+     bs(j) = 0.0
+     do i = 1,numchn
+        if( ECHN(i) .gt. ear(j-1) .and. ECHN(i-1) .le. ear(j) )then
+           E1 = max( ear(j-1) , ECHN(i-1) )
+           E2 = min( ear(j)   , ECHN(i)   )
+           frac = ( E2-E1 ) / ( ECHN(i)-ECHN(i-1) )
+           bs(j) = bs(j) + frac * bkgrate(i)
+        end if
+     end do
+     E = 0.5 * ( ear(j) + ear(j-1) )
+  end do
+  
+! Calculate reference band power (in units of *absolute rms^2*)
+  Pr = pow * getcountrate(Elo,Ehi,nex,earx,rephotarx)
+! Calculate reference band Poisson noise (in *absolutem rms^2)
+  mur = getcountrate(Elo,Ehi,nex,earx,photarx)
+  Prnoise = 2.0 * ( br + mur )
+  write(*,*)"br,mur=",br,mur
+  write(*,*)"Pr (fractional rms)^2/Hz",Pr/mur**2
+  
+! Open file to write the lag simulation to
+  
+  write(*,*)"Enter file name of simulation products"
+  read(*,'(a)')root
+  lagfile =  trim(root) // '.dat'
+  flxlagfile = 'x' // trim(root) // '.dat'
+  phalagfile = 'x' // trim(root) // '.pha'
+  rsplagfile = 'x' // trim(root) // '.rsp'
+  status = 0
+  call ftgiou(xunit,status)
+  open(xunit,file=flxlagfile)
+  call ftgiou(unit,status)
+  open(unit,file=lagfile)
+  
+! Loop through energy bins
+  write(unit,*)"skip on"
+  write(unit,*)"read serr 1 2"
+  do i = 1,ne
+     E  = 0.5 * ( ear(i) + ear(i-1) )
+     dE = ear(i) - ear(i-1)
+     mus = getcountrate(ear(i-1),ear(i),nex,earx,photarx)
+     Psnoise = 2.0 * ( mus + bs(i) )
+     ReG = getcountrate(ear(i-1),ear(i),nex,earx,rephotarx)
+     ImG = getcountrate(ear(i-1),ear(i),nex,earx,imphotarx)
+     G2  = pow**2 * ( ReG**2 + ImG**2 )
+     ! Can finally calculate error     
+     dlag(i) = 1.0 + Prnoise/Pr
+     dlag(i) = dlag(i) * ( G2*(1.0-gammac2) + Psnoise*Pr )
+     dlag(i) = dlag(i) / ( gammac2*G2 )
+     dlag(i) = dlag(i) / ( 2.0 * Texp * (fhi-flo) )
+     dlag(i) = sqrt( dlag(i) )
+     dlag(i) = dlag(i) / ( 2.0 * pi * fc )
+     !Now generate simulated data
+     lagsim(i) = lag(i) + gasdev(idum) * dlag(i)
+     !Write out
+     write(unit,*)E,0.5*dE,lagsim(i),dlag(i),lag(i)
+     write(xunit,*)ear(i-1),ear(i),dE*lagsim(i),dE*dlag(i)
+  end do
+  close(unit)
+  call ftfiou(unit,status)
+  close(xunit)
+  call ftfiou(xunit,status)
+ 
+  command = 'flx2xsp ' // trim(flxlagfile) // ' ' // trim(phalagfile)
+  command = trim(command) // ' ' // trim(rsplagfile)
+  write(*,*)"-----------------------------------------------"
+  write(*,*)"Outputs: ",trim(lagfile),', ',trim(flxlagfile)
+  write(*,*)"command: ",trim(command)
+  write(*,*)"-----------------------------------------------"
+ 
+  return
+end subroutine simrtdbl
+!-----------------------------------------------------------------------
+
+!-----------------------------------------------------------------------
 subroutine simrtdist(ear, ne, param, ifl, photar)
   use telematrix
   implicit none
@@ -465,8 +638,8 @@ subroutine simrtdist(ear, ne, param, ifl, photar)
   pow     = param(26)        !power in [rms/mean]^2/Hz units (alpha(nu))
   par(28) = param(27)        !telescope response
   
-  flo = param(21)
-  fhi = param(22)
+  flo = par(21)
+  fhi = par(22)
   fc  = 0.5 * ( fhi + flo )
   
 ! Get `folded' lags
@@ -483,14 +656,14 @@ subroutine simrtdist(ear, ne, param, ifl, photar)
   
 ! Calculate real and imaginary parts on the fine energy grid
   par(23) = 1.0   !ReIm
-  call genreltrans(Cp, dset, nlp, ear, ne, par, ifl, photar)  
+  call genreltrans(Cp, dset, nlp, earx, nex, par, ifl, rephotarx)  
   par(23) = 2.0   !ReIm
-  call genreltrans(Cp, dset, nlp, ear, ne, par, ifl, photar)  
+  call genreltrans(Cp, dset, nlp, earx, nex, par, ifl, imphotarx)  
 ! Get DC component
   par(21) = 0.0   !floHz
   par(22) = 0.0   !fhiHz
   par(23) = 1.0   !ReIm
-  call genreltrans(Cp, dset, nlp, ear, ne, par, ifl, photar)  
+  call genreltrans(Cp, dset, nlp, earx, nex, par, ifl, photarx)  
 
 ! Read in background array
   if( needbkg )then
@@ -690,7 +863,7 @@ subroutine genreltrans(Cp, dset, nlp, ear, ne, param, ifl, photar)
     save ReSraw,ImSraw,ReSrawa,ImSrawa,ReGrawa,ImGrawa,ReG,ImG
 
     ifl = 1
-    call FNINIT
+    !call FNINIT
 
     ! Initialise some parameters 
     call initialiser(firstcall, Emin, Emax, dloge, earx, rnmax, d, needtrans, me, xe, verbose)
@@ -853,13 +1026,13 @@ subroutine genreltrans(Cp, dset, nlp, ear, ne, param, ifl, photar)
                 write(*,*)"13.6eV-13.6keV luminosity of single source=",ell13pt6
             else
                 call sourcelum(nex,earx,contx,real(mass),real(gso(1)),real(Gamma))
-            end if   
+            end if  
+            if( abs(Cp) .eq. 1 )then
+                write(*,*)"Ecut in source restframe (keV)=",Ecut_s
+            else
+                write(*,*)"kTe in source restframe (keV)=", Ecut_s
+            end if  
         end if         
-        if( abs(Cp) .eq. 1 )then
-            write(*,*)"Ecut in source restframe (keV)=",Ecut_s
-        else
-            write(*,*)"kTe in source restframe (keV)=", Ecut_s
-        end if 
         contx_int = 1. !note: for a single LP we don't need to account for this factor in the ionisation profile, so it's defaulted to 1
         contx = lens(1) * (gso(1)/(real(1.d0+zcos))**Gamma) * contx          
     else 
