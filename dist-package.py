94dde3416e5a6c66d26ef200a50c33ee1fe59727
fjebaker <fergus@cosroe.com>
2026-03-02 09:50:19 +0000

    fix(py): support macos and linux

--- dist-package.py
+++ dist-package.py
@@ -1,20 +1,41 @@
-import subprocess
 import os
+import sys
+import subprocess
 import shutil
-import xspectrampoline_helpers
-from setuptools import Extension, setup
 
-BUILD_DIR = "pybuild"
+import xspectrampoline_helpers
+
+if sys.platform.startswith("linux"):
+    TARGET = "linux"
+    SHARED_EXT = "so"
+elif sys.platform == "darwin":
+    TARGET = "macos"
+    SHARED_EXT = "dylib"
+else:
+    raise Exception(f"Unsupported platform: '{sys.platform}'")
+
+BUILD_DIR = "build"
+TARGET_DIR = "pyreltrans"
 HEADAS = xspectrampoline_helpers.get_HEADAS()
 HEADAS_INCLUDE = os.path.join(HEADAS, "include")
 
 
-def linux_fflags():
-    return ["-shared", "-export-dynamic"]
+def get_ldflags():
+    if TARGET == "linux":
+        return ["-lm", "-lpthread"]
+    elif TARGET == "macos":
+        return ["-lgfortran"]
+    else:
+        raise "unreachable"
 
 
-def linux_ldflags():
-    return ["-lm", "-lpthread"]
+def get_fflags() -> list[str]:
+    if TARGET == "linux":
+        return ["-shared", "-export-dynamic"]
+    elif TARGET == "macos":
+        return ["-dynamiclib"]
+    else:
+        raise "unreachable"
 
 
 def cmd_fortran() -> list[str]:
@@ -32,27 +53,41 @@ def cmd_fortran() -> list[str]:
         # TODO: is this needed?
         "-fopenmp",
     ]
-    cmd += linux_fflags()
+    cmd += get_fflags()
     return cmd
 
 
-def compile(*source_files: str):
-    if os.path.isdir(BUILD_DIR):
-        shutil.rmtree(BUILD_DIR)
-    os.mkdir(BUILD_DIR)
+def compile(*source_files: str) -> str:
+    if not os.path.isdir(BUILD_DIR):
+        os.mkdir(BUILD_DIR)
+
+    if not os.path.isdir(os.path.join(BUILD_DIR, "lib")):
+        os.mkdir(os.path.join(BUILD_DIR, "lib"))
+
+    if os.path.isdir(os.path.join(BUILD_DIR, "cache")):
+        shutil.rmtree(os.path.join(BUILD_DIR, "cache"))
+
     os.mkdir(os.path.join(BUILD_DIR, "cache"))
 
-    wrapper_o = os.path.join(BUILD_DIR, "wrappers.o")
+    output_path = os.path.join(BUILD_DIR, "lib", f"libpyreltrans.{SHARED_EXT}")
+
     cmd = cmd_fortran()
     cmd += source_files
-    cmd += ["-o", os.path.join(BUILD_DIR, "libpyreltrans.so")]
-    cmd += linux_ldflags()
+    cmd += ["-o", output_path]
+    cmd += get_ldflags()
     cmd += xspectrampoline_helpers.get_linker_flags(
         ["XSFunctions", "XSModel", "fftw3", "cfitsio"],
+        # Use relative rpaths so that when it is installed into the Python
+        # envrionment it can find the xspectrampoline libraries.
         rpath_relative=True,
     )
 
     subprocess.run(cmd, check=True)
+    return output_path
 
 
-compile("wrappers.f90")
+if __name__ == "__main__":
+    lib_path = compile("wrappers.f90")
+    print(f"All targets compiled to path '{BUILD_DIR}'")
+    print(f"Copying libpyreltrans.{SHARED_EXT} to {TARGET_DIR}")
+    shutil.copyfile(lib_path, os.path.join(TARGET_DIR, f"libpyreltrans.{SHARED_EXT}"))
