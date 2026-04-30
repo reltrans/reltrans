66bfaff9b1e72a5fe10e6b6eff96c8ef0c0fcf1b
fjebaker <fergus@cosroe.com>
2026-04-30 14:15:08 +0100

    feat: add interfaces for C functions in XSPEC
    Ben and others had noticed some problems when compiling against recent
    versions of HEASoft. It boils down to changing symbol names and
    underscores being inconsistent between the C and Fortran interfaces of
    XSFunctions and e.g. cfitsio. In the past that seems to have not been a
    problem, but on recent OSX it is.
    
    The solution is to actually define the foreign function interfaces in
    the Fortran code, so that the called symbol may be directly assigned
    instead of 'guessed' by the compiler.
    
    For tbabs, that means relying on the C function `C_tbabs`, as it much
    more stable than other variants. This function is however double
    precision, so as a temporary work around a runtime float to double cast
    is performed in a small tbabs wrapper function.
    
    This only affects the tbabs and xsatbl functions, for which the
    interfaces are now added. These should be perhaps moved into a new
    ffi.f90 file or similar, but can live where they are for now.
    
    One small fix also applies to the way that strings are passed. The null
    terminator is now properly concatenated, as previously the trimming
    happened on the C end as well.
    
    Co-Authored-By: Ben Ricketts <b.ricketts@sron.nl>

--- subroutines/genreltrans.f90
+++ subroutines/genreltrans.f90
@@ -2,7 +2,51 @@ module m_genreltrans
     use common_types
     implicit none
 
+    interface
+        ! The interface around the XSPEC C_tbabs function.
+        ! It will call the C_tbabs symbol in the libXSFunctions shared library.
+        subroutine c_tbabs(earx, nex, params, Ifl, absorbx, photerx, str)      &
+            bind(C, name = "C_tbabs")
+            use iso_c_binding, only: c_double, c_int, c_char
+            real(c_double), intent(in) :: earx(nex+1)
+            real(c_double), intent(in) :: params(1)
+            real(c_double), intent(inout) :: absorbx(nex), photerx(nex)
+            integer(c_int), value, intent(in) :: nex, Ifl
+            character(kind=c_char), intent(in) :: str(*)
+        end subroutine c_tbabs
+    end interface
+
 contains
+
+    ! Call the tbabs function from the XSPEC model library.
+    !
+    ! This wrapper temporarily performs a runtime cast on all of the arrays to
+    ! double precision, as the C_tbabs function that we will eventually call
+    ! expects double precision, whilst much of reltrans still uses `real`, wihch
+    ! maps to single precision.
+    !
+    ! Once the precision has been modified, this function can be simplified.
+    subroutine tbabs(earx, nex, nh, Ifl, absorbx, photerx)
+        real, intent(in) :: earx(0:nex), nh
+        real, intent(inout) :: absorbx(nex), photerx(nex)
+        integer, intent(in) :: nex, Ifl
+
+        double precision :: d_earx(0:nex), d_absorbx(nex), d_photerx(nex),     &
+            d_params(1)
+        integer i
+
+        do i = 0,nex
+            d_earx(i) = earx(i)
+        end do
+
+        d_params(1) = nh
+        call c_tbabs(d_earx,nex,d_params,Ifl,d_absorbx,d_photerx, "")
+
+        do i = 1,nex
+            absorbx(i) = d_absorbx(i)
+            photerx(i) = d_photerx(i)
+        end do
+    end subroutine tbabs
 
     ! Allocate the global arrays that reltrans needs
     subroutine setup_global_arrays(config, nlp)
