214680bee418c8efcee950e33bfc3a35ad8621a0
Guglielmo Mastroserio <mgullik@gmail.com>
2026-06-10 13:56:29 +0200

    fix: rebase issues caused problem to the test_basics, in particular rtdist tests

--- test/test_basics.py
+++ test/test_basics.py
@@ -19,9 +19,6 @@ def _debug_plot(
     plt.title(title)
     plt.show()
 
-<<<<<<< HEAD
-
-=======
     
 def _debug_plot_multi_spec(energy, output, title="", xlabel="", ylabel="", yscale="linear", xscale="log"):
     """Used when creating new tests for quickly looking at the data to make
@@ -39,7 +36,6 @@ def _debug_plot_multi_spec(energy, output, title="", xlabel="", ylabel="", yscal
     plt.show()
     
     
->>>>>>> a8ecc0e (fix: call continuum correctly for rtdist model flavour)
 def test_basic_invocation(reltrans, assert_snapshot):
     """A smoke test to check whether the default values are working."""
     reltrans.reset()
@@ -152,22 +148,22 @@ def test_re_im_rtdist(reltrans, assert_snapshot, telescope, envars):
 
     agn1 = rtdist_Parameters(mass=4.7e6, flo_hz=1e-5, fhi_hz=1e-4, re_im=4.0)
     output = reltrans.rtdist(energy, agn1)
-    _debug_plot(energy,output, title = "rtdist lag spectrum", xlabel="Energy [keV]")
+    # _debug_plot(energy,output, title = "rtdist lag spectrum", xlabel="Energy [keV]")
     assert_snapshot(output, name="time_lag")
 
     agn1.re_im = 3
     output = reltrans.rtdist(energy, agn1)
-    _debug_plot(energy,output, title="rtdist modulus spectrum", xlabel="Energy[keV]")
+    # _debug_plot(energy,output, title="rtdist modulus spectrum", xlabel="Energy[keV]")
     assert_snapshot(output, name="magnitude")
 
     agn1.re_im = 1
     output = reltrans.rtdist(energy, agn1)
-    _debug_plot(energy,output, title="rtdist real part spectrum", xlabel="Energy[keV]")
+    # _debug_plot(energy,output, title="rtdist real part spectrum", xlabel="Energy[keV]")
     assert_snapshot(output, name="real_part")
 
     agn1.re_im = 2
     output = reltrans.rtdist(energy, agn1)
-    _debug_plot(energy,output, title="rtdist imaginary part spectrum", xlabel="Energy[keV]")
+    # _debug_plot(energy,output, title="rtdist imaginary part spectrum", xlabel="Energy[keV]")
     assert_snapshot(output, name="imaginary_part")
 
 
@@ -178,11 +174,6 @@ def test_reltransDCp_called_twice_no_resetting(reltrans, assert_snapshot):
     output_dcp2 = reltrans.dcp(energy, DCP_Parameters())
     np.testing.assert_allclose(output_dcp, output_dcp2, rtol=1e-4)
 
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> a8ecc0e (fix: call continuum correctly for rtdist model flavour)
 def test_reltransDCp_called_twice_after_resetting(reltrans, assert_snapshot):
     energy = np.logspace(np.log10(0.1), np.log10(100), 501)
     reltrans.reset()
@@ -192,8 +183,6 @@ def test_reltransDCp_called_twice_after_resetting(reltrans, assert_snapshot):
     np.testing.assert_allclose(output_dcp, output_dcp2, rtol=1e-4)
 
 
-<<<<<<< HEAD
-=======
 def test_rtdist_called_twice(reltrans, assert_snapshot):
     energy = np.logspace(np.log10(0.1), np.log10(100), 501)
     reltrans.reset()
@@ -218,7 +207,6 @@ def test_rtdist_called_twice_lag_and_time_averaged(reltrans, telescope, envars):
     np.testing.assert_allclose(output_rtdist, output_rtdist2, rtol=1e-4)
     
 
->>>>>>> a8ecc0e (fix: call continuum correctly for rtdist model flavour)
 def test_resetting_between_flavours(reltrans):
     energy = np.logspace(np.log10(0.1), np.log10(100), 501)
     reltrans.reset()
@@ -287,72 +275,4 @@ def test_strans_routines_grtrace_outputs(reltrans, assert_snapshot):
     assert_snapshot(re, name="re1", rtol=2e-4)
     assert_snapshot(taudo, name="taudo1", rtol=2e-4)
     assert_snapshot(pem, name="pem1", rtol=2e-4)
-<<<<<<< HEAD
 
-
-# def test_basic_invocation_rtdist(reltrans, assert_snapshot, envars):
-#     """A smoke test to check whether the default values are working."""
-#     reltrans.reset()
-#     energy = np.logspace(np.log10(0.1), np.log10(100), 501)
-#     output = reltrans.rtdist(energy, rtdist_Parameters())
-#     # _debug_plot(energy,output, "rtdist time-averaged spectrum [default parameters]")
-#     assert_snapshot(output)
-
-
-# def test_re_im_rtdist(reltrans, assert_snapshot, telescope, envars):
-#     """Test the re_im parameter to assert that all the different outputs of the
-#     RTDIST model are working. This test requires an RMF and ARF, which is provided by
-#     the `telescope` fixture."""
-#     reltrans.reset()
-#     energy = np.logspace(np.log10(0.1), np.log10(100), 101)
-
-#     envars["RMF_SET"] = telescope.rmf_path
-#     envars["ARF_SET"] = telescope.arf_path
-#     envars["EMIN_REF"] = "0.3"
-#     envars["EMAX_REF"] = "10.0"
-
-#     agn1 = rtdist_Parameters(mass=4.7e6, flo_hz=1e-5, fhi_hz=1e-4, re_im=4.0)
-#     output = reltrans.rtdist(energy, agn1)
-#     # _debug_plot(energy,output, title = "rtdist lag spectrum", xlabel="Energy [keV]")
-#     assert_snapshot(output, name="time_lag")
-
-#     agn1.re_im = 3
-#     output = reltrans.rtdist(energy, agn1)
-#     # _debug_plot(energy,output, title="rtdist modulus spectrum", xlabel="Energy[keV]")
-#     assert_snapshot(output, name="magnitude")
-
-#     agn1.re_im = 1
-#     output = reltrans.rtdist(energy, agn1)
-#     # _debug_plot(energy,output, title="rtdist real part spectrum", xlabel="Energy[keV]")
-#     assert_snapshot(output, name="real_part")
-
-#     agn1.re_im = 2
-#     output = reltrans.rtdist(energy, agn1)
-#     # _debug_plot(energy,output, title="rtdist imaginary part spectrum", xlabel="Energy[keV]")
-#     assert_snapshot(output, name="imaginary_part")
-
-
-# def test_basic_invocation_model_iterations_rtdist(reltrans, assert_snapshot):
-#     energy = np.logspace(np.log10(0.1), np.log10(100), 501)
-#     reltrans.reset()
-#     output_rtdist = reltrans.rtdist(energy, DCP_Parameters())
-#     reltrans.reset()
-#     output_rtdist2 = reltrans.rtdist(energy, DCP_Parameters())
-#     np.testing.assert_allclose(output_rtdist, output_rtdist2, rtol=1e-4)
-
-
-# def test_invocation_model_iterations_rtdist_lag(reltrans, telescope, envars):
-#     reltrans.reset()
-#     envars["RMF_SET"] = telescope.rmf_path
-#     envars["ARF_SET"] = telescope.arf_path
-#     envars["EMIN_REF"] = "0.3"
-#     envars["EMAX_REF"] = "10.0"
-#     energy = np.logspace(np.log10(0.1), np.log10(100), 501)
-#     agn1 = rtdist_Parameters(mass=4.7e6, flo_hz=1e-5, fhi_hz=1e-4, re_im=4.0)
-#     output_rtdist_lag = reltrans.rtdist(energy, agn1)
-#     output_rtdist = reltrans.rtdist(energy, rtdist_Parameters())
-#     reltrans.reset()
-#     output_rtdist_lag2 = reltrans.rtdist(energy, agn1)
-#     np.testing.assert_allclose(output_rtdist_lag, output_rtdist_lag2, rtol=1e-4)
-=======
->>>>>>> a8ecc0e (fix: call continuum correctly for rtdist model flavour)
