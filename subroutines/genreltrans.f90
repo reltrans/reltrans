d9ed35f3e9d6c5f2382f534343e0607dc88a1578
Ben Ricketts <b.ricketts@sron.nl>
2026-06-18 11:00:52 +0200

    style: more reformatting to follow code conventions

--- subroutines/genreltrans.f90
+++ subroutines/genreltrans.f90
@@ -403,8 +403,9 @@ subroutine genreltrans(Cp, dset, nlp, ear, ne, param, ifl, photar)
         config%firstcall = .false.
         config%needtrans = .true.
         config%needconv = .true.
-        prev_nf = 0 !this is needed to reallocate arrays with realloc_arrays, if firstcall is set to true externally
-
+        !this is needed to reallocate arrays with realloc_arrays, if firstcall 
+        !is set to true externally
+        prev_nf = 0 
         ! set sensible distance for observer from the BH
         d = max(1.0d4, 2.0d2 * config%rnmax**2)
 
@@ -471,8 +472,11 @@ subroutine genreltrans(Cp, dset, nlp, ear, ne, param, ifl, photar)
      end if
 
      ! do this for each lamp post, then find some sort of weird average?
-    if (config%verbose .gt. 0) write(*,*)"Observer's reflection fraction for each source:",model_args%boost*frobs
-    if (config%verbose .gt. 0) write(*,*)"Relxill reflection fraction for each source:", frrel
+    if (config%verbose .gt. 0) then
+        write(*,*)"Observer's reflection fraction for each source:",           &
+                    model_args%boost*frobs
+        write(*,*)"Relxill reflection fraction for each source:", frrel
+    end if
 
     if (config%verbose .gt. 2) call CPU_TIME (time_start)
     if (config%needconv)then
