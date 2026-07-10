4ed8a39bfa585935b97a6a8f132b6f13ab392ced
Adam Ingram <a.r.ingram@icloud.com>
2026-07-10 15:58:10 +0100

    Fixed reltransDCp: I found out that nthcomp moronically renormalises a redshifted spectrum to have the same flux as the z=0 spectrum at 1 keV. In getcont.f90 I now undo that monstrosity of a renormalisation.

--- subroutines/continuum/getcont.f90
+++ subroutines/continuum/getcont.f90
@@ -22,28 +22,52 @@
       double precision , intent(in) :: Gamma, zcos
 
       real   , parameter  :: pi = acos(-1.0),ergsev  = 1.602197e-9 ! Convert keV to ergs
-      integer :: i, ifl
+      integer :: i, ifl, j
       real    :: nth_par(5), photer(nex), E, Icomp, inc_flux
       real    :: get_norm_cont_local
+      real    :: contx_base(nex), renorm, dlogE
 
       Icomp = 0.0
 
       if (Cp .eq. 2) then
 !So far this works only with kTe, so only with nthComp continuum model
+
+         !First call with no redshift
+         nth_par(1) = real(Gamma)
+         nth_par(2) = Cutoff_s
+         nth_par(3) = 0.05
+         nth_par(4) = 1.0
+         nth_par(5) = 0.0
+         Ifl=1
+         call donthcomp(earx, nex, nth_par, ifl, contx_base, photer)
+
+         !Now call with correct redshift
          nth_par(1) = real(Gamma)
          nth_par(2) = Cutoff_s
          nth_par(3) = 0.05
          nth_par(4) = 1.0
          nth_par(5) = ( 1.0 + zcos ) / real( gso(1) ) - 1.0
          Ifl=1
-
          call donthcomp(earx, nex, nth_par, ifl, contx, photer)
+         
+         !Calculate the ratio between the two at 1 keV
+         i      = ceiling( -real(nex)*log10(earx(0)) / log10(earx(nex)/earx(0)) )
+         dlogE  = log10(earx(nex)/earx(0)) / real(nex)
+         j      = i + nint( log10((1.+zcos)/gso(1)) / dlogE )
+         renorm = contx(i) / contx_base(j)
+
+         !Take out the ridiculous nthcomp renormalisation
+         contx = contx / renorm
+         !Note: this is needed because nthcomp renormalises a redshifted spectrum to still
+         !have a flux of 1 (or norm within xspec) at 1keV. We need to undo that
+         !catastrophic monstrosity.
+         
 !The continuum needs to be renormalised according to the illuminating flux that was considered in xillver 
 !Plus we divide by a factor that depends on ionisation and density to agree with the first versions of reltrans
          do i = 1, nex
             E   = 0.5 * ( earx(i) + earx(i-1) )
             if (E .ge. 0.1 .and. E .le. 1e3) then
-               Icomp = Icomp + ((earx(i) + earx(i-1)) * 0.5 * contx(i))
+               Icomp = Icomp + ((earx(i) + earx(i-1)) * 0.5 * contx_base(i))
             endif
          enddo
          inc_flux = 10**(logne + logxi) / (4.0 * pi) / ergsev !calculate incident flux in units  [keV/cm^2/s]
